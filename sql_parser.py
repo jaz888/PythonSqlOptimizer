aac6abfe0637642bbf2981cf39fd194c49c60df5
Jieao Zhu <zhujieao@gmail.com>
2016-10-07 02:14:05 -0400

    push

--- sql_parser.py
+++ sql_parser.py
@@ -1,10 +1,10 @@
 import sqlparse
 import pymysql.cursors
-import ConfigParser
+import configparser
 import json
 import sys
 
-configParser = ConfigParser.RawConfigParser()
+configParser = configparser.RawConfigParser()
 configFilePath = r'dbconfig.ini'
 configParser.read(configFilePath)
 db_username = configParser.get('dbconfig', 'username')
@@ -23,5 +23,16 @@ sql = "SELECT DISTINCT p1.developer FROM projects p1, projects p2 WHERE p1.devel
 cursor.execute(sql)
 result = cursor.fetchall()
 for res in result:
-    print(res)
+    pass
+    #print(res)
 connection.close()
+
+
+
+# try to store data in namedtuple
+def select_from_tables_where(sql):
+    parsed = sqlparse.parse(sql)[0]
+    for token in parsed.tokens:
+        print(type(token) , ': ' , token)
+        
+select_from_tables_where('select subtable.name from (select * from person p1, person p2 where p1.name = p2.name and p1.country <> p2.country) sub_table')
