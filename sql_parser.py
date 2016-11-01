3c8357b03c40deeb3fb968f916aba1d69e430281
Jieao Zhu <zhujieao@gmail.com>
2016-11-01 12:26:43 -0400

    select/query will return table instead of pure data

--- sql_parser.py
+++ sql_parser.py
@@ -112,10 +112,28 @@ def query(name_table_mapping, conditions):
     # g = globals()
     # g['name_table_mapping'] = name_table_mapping
     # result = eval("[(s1,s2) for s1 in name_table_mapping['s1'].data for s2 in name_table_mapping['s2'].data  if s1.age==s2.age and s1.name!=s2.name ]", {"name_table_mapping": name_table_mapping})
-    print(query_str)
+    # print(query_str)
     result = eval(query_str, {"name_table_mapping":name_table_mapping, "QUERY":QUERY})
     
-    print(result)
+    res_attributes = []
+    for table_alias in name_table_mapping.keys():
+        for attribute in name_table_mapping[table_name].attributes:
+            res_attributes.append(table_alias+"."+attribute)
+    res_table = table(res_attributes)
+    # print(res_attributes)
+    
+    # fill up table content
+    for item in result:
+        it = iter(item)
+        item_obj = Obj()
+        for x in it:
+            table_alias = x
+            partial_data = next(it)
+            for attribute in name_table_mapping[table_alias].attributes:
+                item_obj.__setattr__(table_alias+"."+attribute, partial_data.__getattribute__(attribute))
+        res_table.data.add(item_obj)
+    return res_table
+            
     
 def generate_query(name_table_mapping, conditions):
     # for s2 in name_table_mapping["s2"] if s2.age == 26
@@ -141,7 +159,7 @@ def join_tables(name_table_mapping):
     
     
 
-#@running_time 
+@running_time 
 def parse(sql):
     parsed = sqlparse.parse(sql)[0]
     token = parsed.tokens[0]
@@ -265,7 +283,17 @@ def select_from(parsed):
     #         newItem.__setattr__(attribute, entry.__getattribute__(attribute))
     #     res_table.data.add(newItem)
     
-    res_table = query(join_table_mapping, conditions)
+    res_with_all_col = query(join_table_mapping, conditions)
+    # print(res_with_all_col.data)
+    res_table = table(res_with_all_col.attributes if isinstance(attributes, str) else attributes)
+    for entry in res_with_all_col.data:
+        newItem = Obj()
+        for attribute in res_table.attributes:
+            newItem.__setattr__(attribute, entry.__getattribute__(attribute))
+        res_table.data.add(newItem)
+    
+    # filter out attributes
+    
     return res_table
         
 
@@ -283,15 +311,19 @@ parse('INSERT INTO student VALUES (8,"Jieao8",26)')
 # selectedData = parse('SELECT name FROM (SELECT * FROM student where id == 2)')
 # selectedData = parse('SELECT name FROM (SELECT * FROM student where id == 2)')
 # selectedData = parse('SELECT name FROM (SELECT * FROM student where id == 2)')
-# selectedData = parse('SELECT * FROM student WHERE age <> 26')
-selectedData = parse('SELECT * FROM student s1, student s2 WHERE s1.age = s2.age AND s1.name <> s2.name')
-
+selectedData = parse('SELECT * FROM student WHERE age <> 26')
+# print(selectedData.data)
+selectedData = parse('SELECT s1.name FROM student s1, student s2 WHERE s1.age = s2.age AND s1.name <> s2.name')
+# print(selectedData.data)
 # selectedData = parse('SELECT name FROM (SELECT * FROM student where id == 2)')
-# print("TOTAL")
-# print("--- %s seconds ---" % time_total)
+
 # print(selectedData.pretty_format())
 # print(DB)
 # print(DB['student'].pretty_format())
 # 'select subtable.name from (select * from person p1, person p2 where p1.name = p2.name and p1.country <> p2.country) sub_table'
 # 'insert into testTable values (1,2,3)'
 # 'CREATE TABLE student (id int, name varchar(255), country varchar(255))'
+
+
+print("TOTAL")
+print("--- %s seconds ---" % time_total)
